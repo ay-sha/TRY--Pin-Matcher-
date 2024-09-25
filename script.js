b9c44b9098caca6a9744fd3af4b8f01e0160c5f8
Aysha <ayshaamin680@gmail.com>
2024-09-25 20:18:05 +0600

    Donee

---
+++ script.js
@@ -0,0 +1,64 @@
+let notifyArea = document.getElementById("notify-section");
+let Match = document.getElementById("match"); 
+let notMatch = document.getElementById("notmatch");
+let generatePinBox = document.getElementsByClassName("generatedPin-Box");
+let buttons = document.querySelectorAll('.button');
+let backspace = document.getElementById("button-backspace"); 
+let clear = document.getElementById("button-clear"); 
+let givenPinInput = document.getElementById("generate-Box"); 
+let pinInput = document.getElementById("Pin-Box"); 
+let pinStr = ""; 
+let newStr = " ";
+
+window.onload = function ()
+{
+    notifyArea.style.display = "none";
+    generatePinBox.value = " "; 
+}
+
+function submitfunc()
+{
+    let givenPin = parseInt(givenPinInput.value);
+    let pin = parseInt(pinInput.value); 
+    if (givenPin == pin)
+    {
+        console.log("Matched");
+        notifyArea.style.display = "block";
+        notMatch.style.display = "none"; 
+    }
+    else
+    {
+        console.log("Don't match");
+        notifyArea.style.display = "block";
+        Match.style.display = "none";
+    }
+   
+}
+
+function getRndInteger(min, max) {
+    console.log("Clicked");
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+buttons.forEach(function(button){
+    button.addEventListener('click', function () {
+        pinStr += this.innerText; 
+        document.getElementById("Pin-Box").value = pinStr;
+        console.log(pinStr);
+    });
+});
+
+clear.addEventListener('click', function ()
+{
+    console.log("clared");
+    document.getElementById("Pin-Box").value = " "; 
+    pinStr = " "; 
+});
+backspace.addEventListener('click', function () {
+
+    newStr = pinStr.slice(0, -1)
+    console.log(newStr);
+    pinStr = newStr; 
+    document.getElementById("Pin-Box").value = pinStr;
+    console.log(pinStr);
+});
